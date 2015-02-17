6237cb1115edc942617e6a571da932a0af3fcb94
Evan Farrell <evandev@gastrograph.com>
2015-02-17 17:14:27 -0500

    Value is int

--- opportunity.go
+++ opportunity.go
@@ -3,7 +3,7 @@ package closeio
 type Opportunity struct {
 	Confidence int `json:"confidence,omitempty"`
 	Status string `json:"status,omitempty"`
-	Value string `json:"value,omitempty"`
+	Value int `json:"value,omitempty"`
 	ValuePeriod string `json:"value_period,omitempty"` //Monthly, annually, one-time
 	Note string `json:"note,omitempty"`
 }
